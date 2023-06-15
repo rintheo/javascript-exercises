4144b20975395784bca87a79d52672e0906038f4
rintheo <107115767+rintheo@users.noreply.github.com>
2023-06-15 16:06:24 +0800

    Complete 03_reverseString following README.md

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    stringReversed = '';
+    for (let i = 0; i < string.length; i++) {
+        stringReversed += string.charAt(string.length -1 - i);
+    }
+    return stringReversed;
 };
 
 // Do not edit below this line
