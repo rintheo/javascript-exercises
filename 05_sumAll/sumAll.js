3985e1eca927cbdc70190bcfa5d2dab5a3e8781a
rintheo <107115767+rintheo@users.noreply.github.com>
2023-06-15 17:25:42 +0800

    Complete 05_sumAll following README.md

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,29 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    let sum = 0;
+    let numStart;
+    let numEnd;
 
+    if (typeof num1 !== 'number' ||
+        typeof num2 !== 'number' ||
+        num1 < 0 ||
+        num2 < 0) { 
+        return 'ERROR';
+    }
+    else {
+        if (num1 < num2) {
+            numStart = num1;
+            numEnd = num2;
+        }
+        else {
+            numStart = num2;
+            numEnd = num1;        
+        }
+    }
+  
+    for (let i = numStart; i <= numEnd; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
