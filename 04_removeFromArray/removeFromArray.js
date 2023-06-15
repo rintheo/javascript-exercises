4d7bd4ced34c234f0b23ef8c1b9da9eb4df061e5
rintheo <107115767+rintheo@users.noreply.github.com>
2023-06-15 16:45:51 +0800

    Complete 04_removeFromArray following README.md

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...args) {
+    
+    for (let i = 0; i < args.length; i++) {
+        for (let j = 0; j < array.length; j++) {
+            if (array[j] === args[i]) {
+                array.splice(j, 1);
+            }
+        }
+    }
 
+    return array;
 };
 
 // Do not edit below this line
