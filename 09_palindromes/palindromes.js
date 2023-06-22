22ec07d0b28a1998b9e77fcc81033a8a7d9908d9
rintheo <107115767+rintheo@users.noreply.github.com>
2023-06-22 21:39:00 +0800

    Complete 09_palindromes following README.md

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    const arr = [];
+    for (let i = 0; i < str.length; i++) {
+        if ((str[i].toUpperCase() != str[i].toLowerCase()) || 
+           (Number.isInteger(parseInt(str[i])))) {
+            arr.push(str[i].toLowerCase());
+        }
+    }
+    return arr.join('') === arr.reverse().join('');
 };
 
 // Do not edit below this line
