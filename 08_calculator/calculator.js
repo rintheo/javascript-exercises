bee3310b5357190bc4434823e5ab58409d82de87
rintheo <107115767+rintheo@users.noreply.github.com>
2023-06-22 19:28:13 +0800

    Complete 08_calculator following README.md

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,28 @@
-const add = function() {
-	
+const add = function(...nums) {
+	return nums.reduce((total, num) => total + num, 0);
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(nums) {
+	return nums.reduce((total, num) => total + num, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(...nums) {
+  return nums.reduce((total, num) => total * num, 1);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) { // num = 3
+	// const factorial = [];
+  // for (i = num; i > 0; i--) factorial.push(i);
+  // return factorial.reduce((result, num) => result * num, 1);
+  return (num > 0) ? num * factorial(num - 1) : 1; // ternary if...else recursion 
 };
 
 // Do not edit below this line
