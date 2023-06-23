86d322a3ab513f38129ebcce549b5e242c57a01e
rintheo <107115767+rintheo@users.noreply.github.com>
2023-06-23 13:18:46 +0800

    Complete 11_getTheTitle following README.md

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,10 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(array) {
+    // const book = [];
+    // for (item of array) {
+    //     book.push(item.title);
+    // }
+    // return book;
+    return array.map((item) => item.title);
 };
 
 // Do not edit below this line
