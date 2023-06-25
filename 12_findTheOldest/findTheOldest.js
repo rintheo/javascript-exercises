bb363386eb740159a3cf6092489a6fd36658de67
rintheo <107115767+rintheo@users.noreply.github.com>
2023-06-25 18:27:31 +0800

    Complete 12_findTheOldest following README.md

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,14 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(array) {
+  yearCurrent = new Date().getFullYear();
+  return array.sort((a, b) => {
+    for (person of [a, b]) {
+      person.yearLatest = (person.hasOwnProperty('yearOfDeath')) ?
+                           person.yearOfDeath : yearCurrent;
+    }
+    return (b.yearLatest - b.yearOfBirth) - (a.yearLatest - a.yearOfBirth);
+  })[0];
 };
+
 
 // Do not edit below this line
 module.exports = findTheOldest;
